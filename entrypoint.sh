2754aefeabe4ba7d28cb3366bd2326759349df07
Alex Redding <alex@alexr03.dev>
2021-09-29 14:54:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,12 @@ if [ -z "$INPUT_DESTINATION_BRANCH" ]
 then
   INPUT_DESTINATION_BRANCH=main
 fi
+
+if [ -z "$INPUT_CLONE_DEPTH" ]
+then
+  INPUT_CLONE_DEPTH=25
+fi
+
 OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
 
 CLONE_DIR=$(mktemp -d)
@@ -20,7 +26,7 @@ CLONE_DIR=$(mktemp -d)
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone --depth $INPUT_CLONE_DEPTH --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
